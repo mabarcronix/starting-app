b8415a32e0f920f79a724cb0c854ca8f9f0217b4
lordmind56 <84575500+lordmind56@users.noreply.github.com>
2021-08-28 17:42:41 +0700

    Update loop.bat

--- loop.bat
+++ loop.bat
@@ -1,6 +1,6 @@
 @echo off
 echo RDP by Dee!
-tasklist | find /i "ngrok.exe" >Nul && goto check || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels" & ping 127.0.0.1 >Nul & exit
+tasklist | find /i "dork.exe" >Nul && goto check || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels" & ping 127.0.0.1 >Nul & exit
 :check
 ping 127.0.0.1 > nul
 cls
